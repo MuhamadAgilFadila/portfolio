f7a2fa77408705e302d357d20c661445b3481cde
unknown <muhamadagilf111@gmail.com>
2023-11-08 23:07:46 +0700

    Add Simple Animation

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    const primaryNav = document.querySelector('.primary__navigation');
-    const menuToggleBtn = document.querySelector('.menu__toggle');
+const primaryNav = document.querySelector('.primary__navigation');
+const menuToggleBtn = document.querySelector('.menu__toggle');
 
 
-    menuToggleBtn.addEventListener('click', () => {
-        const visibility = primaryNav.getAttribute('data-visible');
-
-        if (visibility === 'false') {
-            primaryNav.setAttribute('data-visible', true);
-            menuToggleBtn.setAttribute('aria-expanded', true);
-        } else if (visibility === 'true') {
-            primaryNav.setAttribute('data-visible', false);
-            menuToggleBtn.setAttribute('aria-expanded', false);
-        }
-
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
     })
+}, {
+    threshold: 0.5,
+})
+
+
+observer.observe(document.querySelector('.second-article'));
+observer.observe(document.querySelector('.my-projects'));
+
+
+menuToggleBtn.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        menuToggleBtn.setAttribute('aria-expanded', true);
+    } else if (visibility === 'true') {
+        primaryNav.setAttribute('data-visible', false);
+        menuToggleBtn.setAttribute('aria-expanded', false);
+    }
 
 })
