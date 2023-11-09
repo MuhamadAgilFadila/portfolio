00a1193f26a887d6d683e10e65823c04998b0da0
unknown <muhamadagilf111@gmail.com>
2023-11-09 09:36:52 +0700

    Add Simple Animations

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const primaryNav = document.querySelector('.primary__navigation');
 const menuToggleBtn = document.querySelector('.menu__toggle');
 
+const projectBoxes = document.querySelectorAll('.project-box');
+
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle("show", entry.isIntersecting)
         if (entry.isIntersecting) observer.unobserve(entry.target)
     })
-}, {
-    threshold: 0.5,
-})
-
+}, { threshold: 1, })
 
 observer.observe(document.querySelector('.second-article'));
 
+projectBoxes.forEach(box => {
+    observer.observe(box)
+})
 
 
 menuToggleBtn.addEventListener('click', () => {
